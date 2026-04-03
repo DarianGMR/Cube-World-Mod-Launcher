a0458686bb6857c7d88c75b72ef38fb413745a3b
DarianGMR <167760447+DarianGMR@users.noreply.github.com>
2026-04-03 08:44:34 -0400

    Enhance Process class with new methods and member
    Added Wait and Cleanup methods, and a vector to manage injected threads.

--- Process.h
+++ Process.h
@@ -3,11 +3,15 @@
 
 #include <string>
 #include <windows.h>
+#include <vector>
 
 class Process
 {
 public:
+    // Constructor
     Process(const std::string& executablePath);
+    
+    // Destructor
     virtual ~Process();
 
     // Crear proceso suspendido
@@ -19,6 +23,9 @@ public:
     // Reanudar proceso principal
     void Resume();
     
+    // Esperar a que el proceso termine
+    void Wait();
+    
     // Obtener handle del proceso
     HANDLE GetProcessHandle() const { return pi.hProcess; }
     
@@ -29,9 +36,13 @@ private:
     std::string executablePath;
     STARTUPINFOA si;
     PROCESS_INFORMATION pi;
+    std::vector<HANDLE> injectedThreads;
 
     // Función auxiliar para escribir un JMP
     void WriteJMP(BYTE* location, BYTE* newFunction);
+    
+    // Limpiar recursos
+    void Cleanup();
 };
 
 #endif // PROCESS_H
